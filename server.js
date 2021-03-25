070515324e90ae43725c307fd1bfcadf4bd23a2f
melipi <melissapita@gmail.com>
2021-03-25 09:53:26 -0400

    Git repository#	new file:   node_modules/qs/test/index.js Face Detect [:)] API Backend
    This is the backend to the Face Detect App which uses clarafai's API to detect human faces in images.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '***REMOVED***',
+        user : '***REMOVED***',
+        password : '***REMOVED***',
+        database : 'facerecon'
+    }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt, saltRounds))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', image.handleApiCall)
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
