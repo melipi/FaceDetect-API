0f481ea8796596da193d2192b3f522a429aeee22
melipi <melissapita@gmail.com>
2021-03-26 10:00:47 -0400

    adding dynamic port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id', profile.handleProfileGet(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', image.handleApiCall)
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
