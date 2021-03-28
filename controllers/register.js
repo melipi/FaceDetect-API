1f01c7a3596bdcea3c8d1ceac6db540e5e5b5156
melipi <melissapita@gmail.com>
2021-03-28 13:44:53 -0400

    converted to async bcrypt

--- controllers/register.js
+++ controllers/register.js
@@ -1,32 +1,38 @@
 const handleRegister = (db, bcrypt, saltRounds) => (req, res) => {
     const { email, name, password } = req.body;
+
     if(!email || !name || !password) {
         return res.status(400).json('Incorrect form submission')
     }
-    const hash = bcrypt.hashSync(password, saltRounds);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
+
+    bcrypt.hash(password, saltRounds)
+        .then(function(hash) {
+            // store hash in login db
+            db.transaction(trx => {
+                trx.insert({
+                    hash: hash,
+                    email: email
+                })
+                .into('login')
+                .returning('email')
+                .then(loginEmail => {
+                    return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                })
+                .then(trx.commit)
+                .then(console.log('hash', hash))
+                .catch(trx.rollback)
             })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })    
-    .catch(err => res.status(400).json('Unable to register'))
+        })    
+        .catch(err => res.status(400).json('Unable to register'))
 }
 
 module.exports = {
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,29 +1,36 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
     const { email, password } = req.body;
+
     if(!email || !password) {
         return res.status(400).json('Incorrect form submission')
+    } else {
+        checkUser(email, password);
     }
-    db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-        console.log(data)
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        console.log('const', isValid)
-        if(isValid) {
-            console.log('True', isValid)
-            return db.select('*'). from('users')
-             .where('email', '=', email)
-             .then(user => {
-                 res.json(user[0])
-             })
-             .catch(err => res.status(400).json('Error logging in'))
-        } else {
-            console.log('False', isValid)
-            res.status(400).json('Sorry, invalid username or password')
-        }
-    })
-    .catch(err => res.status(400).json('Invalid username or password!'))
- }
+
+    async function checkUser(email, password) {
+        //... fetch user from a db etc.
+        db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            console.log('check data', data)
+        
+            const match = await bcrypt.compare(password, user.passwordHash);
+            
+            if(match) {
+                //login
+                return db.select('*'). from('users')
+                .where('email', '=', email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('Error logging in'))
+            } else {
+                res.status(400).json('hash doesn\'t match db')
+            } 
+        })
+        .catch(err => res.status(400).json('Invalid email or password!'))
+    }
+}
 
  module.exports = {
      handleSignin
