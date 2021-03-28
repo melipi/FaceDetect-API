b0937785607294f605eb8feacaaca48f4a518ad0
melipi <melissapita@gmail.com>
2021-03-27 20:42:30 -0400

    updating database to heroku, connectionString for Node.js

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '***REMOVED***',
-        user : '***REMOVED***',
-        password : '***REMOVED***',
-        database : 'facerecon'
+        connectionString : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
