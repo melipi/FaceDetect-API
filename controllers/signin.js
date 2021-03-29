0677312a981b651789a3f58d47b3a5cdbd6100e1
melipi <melissapita@gmail.com>
2021-03-28 22:18:29 -0400

    signin route cleanup

--- controllers/signin.js
+++ controllers/signin.js
@@ -6,26 +6,29 @@ const handleSignin = (db, bcrypt) => (req, res) => {
     db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
-        console.log('check data', data)
+        if (data.length) {
+            console.log('check data', data)
 
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-
-        console.log('check const', isValid)
-
-        if(isValid) {
-            console.log('True', isValid)
-            return db.select('*'). from('users')
-             .where('email', '=', email)
-             .then(user => {
-                 res.json(user[0])
-             })
-             .catch(err => res.status(400).json('Error logging in'))
-        } else {
-            console.log('False', isValid)
-            res.status(400).json('Sorry, invalid username or password')
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+    
+            console.log('check const', isValid)
+    
+            if(isValid) {
+                console.log('True', isValid)
+                return db.select('*')
+                    .from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('1 - Error, something went wrong'))
+            } else {
+                console.log('False', isValid)
+                res.status(400).json('2 - Invalid username or password!')
+            }
         }
     })
-    .catch(err => res.status(400).json('Invalid username or password!'))
+    .catch(err => res.status(400).json('4 - Invalid username or password!'))
  }
 
  module.exports = {
