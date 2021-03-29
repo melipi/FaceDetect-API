ce17d1a089eae312bb6cac6b04dffc355032fce1
melipi <melissapita@gmail.com>
2021-03-29 11:57:36 -0400

    removed console logs from debugging signin route

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,31 +1,35 @@
 const handleSignin = (db, bcrypt) => async (req, res) => {
     const { email, password } = req.body;
-    console.log(req.body)
+
+    // Validate empty submissions
     if(!email || !password) {
-        return res.status(400).json('Incorrect form submission')
+        return res.status(400).json('Email and password are required for sign in')
     }
-    db
-    .select('email', 'hash')
+
+    // login
+    db.select('email', 'hash')
     .from('login')
     .where('email', '=', email)
     .then (async data => {
+        //check password against hash
         const isValid = await bcrypt.compare(password, data[0].hash);
         if (isValid) {
             try {
+                //true
                 return db.select('*').from('users')
                 .where('email', '=', email)
                 .then(user => {
                   res.json(user[0])
                 })
             } catch (err) {
-                res.status(400).json('Failed to fetch the user');
+                res.status(400).json('Failed to fetch the user from db');
             }
         } else {
-            res.status(400).json('Invalid credentials');
+            res.status(400).json('Invalid credentials, please try again');
         }
     })
     .catch(err => {
-        res.status(400).json('failed to login');
+        res.status(400).json('Login failed');
     })
 }
 
