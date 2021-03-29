18db975b6796db2aefd6989d0e9df658634db41e
melipi <melissapita@gmail.com>
2021-03-28 22:08:02 -0400

    rollback to sync

--- controllers/register.js
+++ controllers/register.js
@@ -1,38 +1,32 @@
 const handleRegister = (db, bcrypt, saltRounds) => (req, res) => {
     const { email, name, password } = req.body;
-
     if(!email || !name || !password) {
         return res.status(400).json('Incorrect form submission')
     }
-
-    bcrypt.hash(password, saltRounds)
-        .then(function(hash) {
-            // store hash in login db
-            db.transaction(trx => {
-                trx.insert({
-                    hash: hash,
-                    email: email
-                })
-                .into('login')
-                .returning('email')
-                .then(loginEmail => {
-                    return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-                })
-                .then(trx.commit)
-                .then(console.log('hash', hash))
-                .catch(trx.rollback)
+    const hash = bcrypt.hash(password, saltRounds); //sync
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
             })
-        })    
-        .catch(err => res.status(400).json('Unable to register'))
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })    
+    .catch(err => res.status(400).json('Unable to register'))
 }
 
 module.exports = {
