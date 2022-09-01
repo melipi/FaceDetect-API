143a7f7f938a204103f36b1b4124d08e5f580860
melipi <melissapita@gmail.com>
2022-09-01 15:25:17 -0400

    Adding CORS credentials to server.js

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Fix for CORS issue
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', (req, res) => { res.send('everything is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt, saltRounds))
