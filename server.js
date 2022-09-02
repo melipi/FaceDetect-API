3e305e171a9a496b416c8aaa8d491ebf50521de0
melipi <melissapita@gmail.com>
2022-09-02 12:56:51 -0400

    rollback cors settings, making a new app on Heroku

--- server.js
+++ server.js
@@ -25,15 +25,15 @@ app.use(cors());
 app.use(express.json());
 
 // Fix for CORS issue
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
-app.get('/', (req, res) => { res.send('everything is working!') })
+app.get('/', (req, res) => { res.send('Server up and running :)') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt, saltRounds))
 app.get('/profile/:id', profile.handleProfileGet(db))
@@ -41,5 +41,5 @@ app.put('/image', image.handleImage(db))
 app.post('/imageurl', image.handleApiCall)
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
