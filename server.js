fba93d6ef369ba1cf81cb898ee7a840e7e2e9f57
melipi <melissapita@gmail.com>
2023-06-01 22:03:09 -0400

    update to neondb

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-        min: 0
-    }
+    searchPath: ['knex', 'public']
 });
 
 const app = express();
